33412fccce93ce25bf6a465f55194f66cb4e5691
Igor Vasyo <igor.vasyo.1999@ukr.net>
2017-03-19 02:07:59 +0200

    Add Stream Music

--- app/src/main/java/theunderdog/radioskovoroda/MainActivity.java
+++ app/src/main/java/theunderdog/radioskovoroda/MainActivity.java
@@ -53,3 +53,110 @@ public class MainActivity extends AppCompatActivity {
             }
         } class PlayerTask {
     }
+
+
+    /*
+    package com.example.igorko.streammusic;
+
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.IOException;
+
+public class StreamMusic extends AppCompatActivity {
+    Button b_music;
+
+    MediaPlayer mediaPlayer;
+
+    boolean prepared = false;
+    boolean started = false;
+    String stream = "http://radioskovoroda.com/music";
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stream_music);
+        b_music = (Button) findViewById(R.id.b_music);
+        b_music.setEnabled(false);
+        b_music.setText("LOADING");
+
+        mediaPlayer = new MediaPlayer ();
+        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+
+        new PlayerTask().execute(stream);
+
+        b_music.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(started){
+                    started = false;
+                    mediaPlayer.pause();
+                    b_music.setText("PLAY");
+
+            } else {
+                    started = true;
+                    mediaPlayer.start();
+                    b_music.setText("PAUSE");
+                }
+
+                }
+        });
+    }
+
+
+    class PlayerTask extends AsyncTask<String , Void , Boolean > {
+        @Override
+        protected Boolean doInBackground(String... strings) {
+
+            try {
+                mediaPlayer.setDataSource(strings[0]);
+                mediaPlayer.prepare();
+                prepared = true;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return prepared;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean aBoolean) {
+            super.onPostExecute(aBoolean);
+            b_music.setEnabled(true);
+            b_music.setText("MUSIC");
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(started){
+            mediaPlayer.pause();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(started){
+            mediaPlayer.start();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(prepared){
+            mediaPlayer.release();
+        }
+    }
+}
+
+     */

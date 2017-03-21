5c94f43a17ec96af0d6c2204f0c7cb3c495a65d3
mooniamsexydude <mooniamsexydude1@gmail.com>
2017-03-21 09:43:49 +0200

    dimens fix, new activity

--- app/src/main/java/theunderdog/radioskovoroda/MainActivity.java
+++ app/src/main/java/theunderdog/radioskovoroda/MainActivity.java
@@ -28,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
 
         b_play = (Button) findViewById(R.id.b_play);
         b_play.setEnabled(false);
-        b_play.setText("LOADING");
+        b_play.setText(R.string.loading_status);
 
         mediaPlayer = new MediaPlayer();
         mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);

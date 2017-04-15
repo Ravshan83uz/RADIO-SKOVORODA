44701ac418790d1b4905d63e9003fce6144a2011
Vasyl Zelinskiy <vasiazel96@gmail.com>
2017-04-15 21:25:31 +0300

    SeekBar(Volume button)

---
+++ app/src/main/java/com/radioskovoroda/SeekBarActivity.java
@@ -0,0 +1,30 @@
+package com.radioskovoroda;
+
+import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.Toast;
+public class SeekBarActivity extends MainActivity  implements SeekBar.OnSeekBarChangeListener {
+    SeekBar seekBar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+
+        seekBar=(SeekBar)findViewById(R.id.seekBar);
+        seekBar.setOnSeekBarChangeListener(this);
+    }
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress,
+                                  boolean fromUser) {
+        Toast.makeText(getApplicationContext(),"seekbar progress: "+progress, Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+        Toast.makeText(getApplicationContext(),"seekbar touch started!", Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+        Toast.makeText(getApplicationContext(),"seekbar touch stopped!", Toast.LENGTH_SHORT).show();
+    }
+
+    }
